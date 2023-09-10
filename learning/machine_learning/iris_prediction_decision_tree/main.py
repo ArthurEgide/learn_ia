e733084060520e43866eb18153d6cb6c9f610bf8
Arthur Egide <arthur@tokenology.com>
2023-09-09 21:23:11 -0300

    Add plot of decisions

--- learning/machine_learning/iris_prediction_decision_tree/main.py
+++ learning/machine_learning/iris_prediction_decision_tree/main.py
@@ -2,12 +2,9 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-
+from sklearn import tree
 import pandas as pd
-import matplotlib
-import seaborn as sns
-import numpy as np
-import plotly.express as px
+import matplotlib.pyplot as plt
 
 PLOT_BASE_PATH = 'plots'
 
@@ -28,7 +25,7 @@ dataset_test_percent = 33.3
 random_seed = 42
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=dataset_test_percent*(10**-2), random_state=random_seed)
 
-dtree = DecisionTreeClassifier(criterion='entropy', max_depth=4)
+dtree = DecisionTreeClassifier(criterion='entropy', max_depth=4, splitter='best')
 dtree.fit(X_train, y_train)
 
 y_pred_train = dtree.predict(X_train)
@@ -40,7 +37,8 @@ scikit_score_test = dtree.score(X_train, y_train)
 cm = confusion_matrix(y_test, y_pred_test, labels= dtree.classes_)
 
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=iris.target_names)
-disp.plot().figure_.savefig(f'{PLOT_BASE_PATH}/decision_tree_1.png',dpi=300)
+disp.plot().figure_.savefig(f'{PLOT_BASE_PATH}/decision_tree_confusion_matrix.png',dpi=300)
 
-scikit_score_train
-scikit_score_test
+plt.figure(figsize=(20,15))
+tree.plot_tree(dtree, fontsize=10)
+plt.savefig(f'{PLOT_BASE_PATH}/decision_tree', dpi=300)
